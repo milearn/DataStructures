1b97b2c1141cdf08d5a6aaa5c8701e2c2744a850
milan gupta <milan.g@flipkart.com>
2021-05-28 09:05:17 +0530

    add minHeap implementation

---
+++ heap/heap.js
@@ -0,0 +1,77 @@
+class MinHeap {
+  heap = [];
+
+  insert(val) {
+    if (typeof val === "undefined" || val === null) {
+      return;
+    }
+    this.heap.push(val);
+    const getParentInd = (child) => {
+      return Math.floor((child - 1) / 2);
+    };
+    if (this.heap.length > 1) {
+      let childInd = this.heap.length - 1;
+      let parentInd = getParentInd(childInd);
+      while (parentInd >= 0 && this.heap[parentInd] > this.heap[childInd]) {
+        [this.heap[parentInd], this.heap[childInd]] = [
+          this.heap[childInd],
+          this.heap[parentInd],
+        ];
+        childInd = parentInd;
+        parentInd = getParentInd(childInd);
+      }
+    }
+  }
+  remove() {
+    const heap = this.heap;
+    if (!this.heap.length) {
+      return null;
+    }
+    if (this.heap.length < 2) {
+      this.heap.shift();
+    } else {
+      //   let largest = this.heap[0];
+      this.heap[0] = this.heap[this.heap.length - 1];
+      this.heap.pop();
+
+      let current = 0;
+      let leftChildIndex = 2 * current + 1;
+      let rightChildIndex = leftChildIndex + 1;
+      while (
+        leftChildIndex < this.heap.length &&
+        (this.heap[current] > this.heap[leftChildIndex] ||
+          this.heap[current] > this.heap[rightChildIndex])
+      ) {
+        if (this.heap[current] > this.heap[leftChildIndex]) {
+          [this.heap[current], this.heap[leftChildIndex]] = [
+            this.heap[leftChildIndex],
+            this.heap[current],
+          ];
+          current = leftChildIndex;
+        } else {
+          [this.heap[current], this.heap[rightChildIndex]] = [
+            this.heap[rightChildIndex],
+            this.heap[current],
+          ];
+          current = rightChildIndex;
+        }
+        leftChildIndex = 2 * current + 1;
+        rightChildIndex = leftChildIndex + 1;
+      }
+    }
+  }
+}
+
+(function () {
+  const minHeap = new MinHeap();
+  minHeap.insert(6);
+  minHeap.insert(5);
+  minHeap.insert(10);
+  minHeap.insert(2);
+  minHeap.insert(7);
+  minHeap.insert(8);
+  minHeap.insert(3);
+  console.log(minHeap.heap);
+  minHeap.remove();
+  console.log(minHeap.heap);
+})();

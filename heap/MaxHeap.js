0b09da9838b3929959f37eead56f339c7f284149
milan gupta <milan.g@flipkart.com>
2021-05-29 08:30:23 +0530

    creates MaxHeap and fixes for MinHeap

--- heap/MaxHeap.js
+++ heap/MaxHeap.js
@@ -1,19 +1,21 @@
-class MinHeap {
+class MaxHeap {
   heap = [];
-
   push(val) {
     if (typeof val === "undefined" || val === null) {
-      return;
+      return undefined;
     }
     this.heap.push(val);
+    if (this.heap.length === 1) {
+      return;
+    }
     const getParentInd = (child) => {
       return Math.floor((child - 1) / 2);
     };
 
     let childInd = this.heap.length - 1;
     let parentInd = getParentInd(childInd);
-    // start from bottom and re-heapify till min heap condition of parent > child is satisfied
-    while (parentInd >= 0 && this.heap[parentInd] > this.heap[childInd]) {
+    // start from bottom and re-heapify till min heap condition of parent < child is satisfied
+    while (parentInd >= 0 && this.heap[parentInd] < this.heap[childInd]) {
       [this.heap[parentInd], this.heap[childInd]] = [
         this.heap[childInd],
         this.heap[parentInd],
@@ -29,7 +31,7 @@ class MinHeap {
     if (!this.heap.length) {
       return null;
     }
-    let smallest = this.heap[0];
+    let largest = this.heap[0];
     if (this.heap.length < 2) {
       this.heap.shift();
     } else {
@@ -38,43 +40,52 @@ class MinHeap {
       this.heap.pop();
       // re-heapify starting from top root element.
       // re-heapify if either of the child is less than parent.
-      // choose the smallest element among the child and swap it with parent
+      // choose the largest element among the child and swap it with parent
       let current = 0;
       let leftChildIndex = 2 * current + 1;
       let rightChildIndex = leftChildIndex + 1;
       while (
         leftChildIndex < this.heap.length &&
         rightChildIndex < this.heap.length &&
-        (this.heap[current] > this.heap[leftChildIndex] ||
-          this.heap[current] > this.heap[rightChildIndex])
+        (this.heap[current] < this.heap[leftChildIndex] ||
+          this.heap[current] < this.heap[rightChildIndex])
       ) {
-        const smallestChildIndex =
-          this.heap[leftChildIndex] < this.heap[rightChildIndex]
+        const largestChildIndex =
+          this.heap[leftChildIndex] > this.heap[rightChildIndex]
             ? leftChildIndex
             : rightChildIndex;
-        [this.heap[smallestChildIndex], this.heap[current]] = [
+        [this.heap[largestChildIndex], this.heap[current]] = [
           this.heap[current],
-          this.heap[smallestChildIndex],
+          this.heap[largestChildIndex],
         ];
-        current = smallestChildIndex;
+        current = largestChildIndex;
         leftChildIndex = 2 * current + 1;
         rightChildIndex = leftChildIndex + 1;
       }
     }
-    return smallest;
+    return largest;
   }
 }
 
 (function () {
-  const minHeap = new MinHeap();
-  minHeap.push(6);
-  minHeap.push(5);
-  minHeap.push(10);
-  minHeap.push(2);
-  minHeap.push(7);
-  minHeap.push(8);
-  minHeap.push(3);
-  console.log(minHeap.heap);
-  minHeap.pop();
-  console.log(minHeap.heap);
+  console.log("---------MAX HEAP---------");
+  const maxHeap = new MaxHeap();
+  maxHeap.push(6);
+  maxHeap.push(5);
+  maxHeap.push(10);
+  maxHeap.push(2);
+  maxHeap.push(7);
+  maxHeap.push(8);
+  maxHeap.push(3);
+  console.log(maxHeap.heap);
+  maxHeap.pop();
+  console.log(maxHeap.heap);
+  maxHeap.pop();
+  console.log(maxHeap.heap);
+  maxHeap.pop();
+  console.log(maxHeap.heap);
+  maxHeap.pop();
+  console.log(maxHeap.heap);
+  maxHeap.pop();
+  console.log(maxHeap.heap);
 })();
